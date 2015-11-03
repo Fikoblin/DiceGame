400eef8282e251e5fce45254b3d4eb2ef9b392a5
Fikoblin <mateuszmeller@hotmail.com>
2015-11-03 13:24:46 +0100

    Revert "wyjatki"
    This reverts commit 259d996f02b02e94dcf79cbf73df5c1fe1e74d0a.

--- src/dicegame/players/Player.java
+++ src/dicegame/players/Player.java
@@ -3,7 +3,6 @@ package dicegame.players;
 public abstract class Player {
 
     public abstract int guess();
-    
     private String name;
 
     public Player() {
@@ -13,16 +12,16 @@ public abstract class Player {
         this.name = name;
     }
 
-   
-    public void setName(String name){ 
- if(name!=null && !name.isEmpty() ) {
-            this.name = name;
- } else {System.err.println("podaj  nazwe urzytkowanika");
-         this.name="Anonim";}
-
-    }
-     public String getName() {
+    public String getName() {
         return name;
     }
 
+    public final void setName(String name) {
+        if (name != null && !name.isEmpty()) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+    }
 }
