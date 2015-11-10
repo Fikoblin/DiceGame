8fd532cbcc7ed900f0afcb022d56d37393175c95
Mateusz Meller <mmeller@us.edu.pl>
2015-11-10 13:39:17 +0100

    1.1

--- src/dicegame/DiceGame.java
+++ src/dicegame/DiceGame.java
@@ -18,15 +18,11 @@ public class DiceGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
-        Player gracz = new PlayerComputer("Ziutek");
         Game game = new Game();
-        
-        try {
-        game.setPlayer(null);
+        Player gracz = new PlayerComputer("Ziutek");
+        game.addPlayer(gracz);
         game.play();
-        } catch (IllegalArgumentException ex){
-            System.err.println(ex.getMessage());
-        }
+
+
     }
 }
--- src/dicegame/Game.java
+++ src/dicegame/Game.java
@@ -5,7 +5,8 @@
 package dicegame;
 
 import dicegame.players.Player;
-import java.awt.List;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -16,21 +17,21 @@ public class Game {
     
     private Player player;
     
+    private List<Player> players = new ArrayList<>();
+    
     //List lista = new List();
 
-    int losowanie() {
-        Random los = new Random();
-        return los.nextInt(6) + 1;
-    }
 
     public void play() {
+        
+        Random dice = new Random();
 
        int zgad = 0, rzut = 0;
         do {
             System.out.println("Rzucam kością");
-            rzut = losowanie();
-            System.out.print(player.getName() + ": ");
-            zgad = player.guess();
+            rzut = dice.nextInt(6)+1;
+            System.out.print(players.get(0).getName() + ": ");
+            zgad = players.get(0).guess();
             System.out.println(zgad);
             if (rzut == zgad) {
                 System.out.println("Zgadłeś! Wylosowana liczba to " + rzut);
@@ -50,5 +51,9 @@ public class Game {
         else {
             throw new IllegalArgumentException("Player nie może być null");
         }
+    }
+    
+    public void addPlayer(Player player) {
+        players.add(player);
     }
 }
